6c4d3b45ae4d38d4dae767af2f3e151a0115a04c
xsalles <xs.salles@gmail.com>
2024-06-21 17:58:26 -0300

    refactoreed: changed the img from profile side and changed too img from background side

--- src/components/pattern/composition/post.composition.jsx
+++ src/components/pattern/composition/post.composition.jsx
@@ -3,7 +3,7 @@ import styles from "../../styles.module.css"
 export default function Post(){
     return(
         <div className={styles.postContainer}>
-            
+            <h1>Sales</h1>
         </div>
     )
 }
--- src/components/pattern/composition/sideBar.composition.jsx
+++ src/components/pattern/composition/sideBar.composition.jsx
@@ -1,11 +1,12 @@
 import styles from "../../styles.module.css"
-import BackgroundSide from "../../../assets/images/background-sidebar.png"
+import BackgroundSide from "../../../assets/images/background-sidebar.jpg"
 import Profile from "../../../assets/images/Profile.png"
 
 export default function SideBar(){
     return(
         <div className={styles.sidebarContainer}>
             <img  className={styles.backgroundSide} src={BackgroundSide} alt="" />
+            <img className={styles.profileSide} src="https://avatars.githubusercontent.com/u/160802244?v=4" alt="" />
             <h1>Pedro Sales</h1>
             <p>Dev Front-End</p>
             <button className={styles.lineSide}></button>
